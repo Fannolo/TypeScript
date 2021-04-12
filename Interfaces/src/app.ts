443c282461226308e32485a8a711d020facbb13d
alessandro <alessandro.fan@welld.ch>
2021-04-12 11:43:21 +0200

    feat: optional properties

--- Interfaces/src/app.ts
+++ Interfaces/src/app.ts
@@ -1,8 +1,7 @@
 interface Named {
-  readonly name: string;
-  outputName?: string;
+  readonly name?: string;
+  outputName?: string; // optional property
 }
-
 interface Aged {
   readonly age: number;
 }
@@ -10,28 +9,30 @@ interface Greetable extends Named, Aged {
   // readonly name: string;
   readonly greet: (phrase: string) => void;
 }
-
 interface AddFn {
   (a: number, b: number): number;
 }
 
 let add: AddFn;
-
 add = (a: number, b: number) => {
   return a + b;
 };
 
-
-
 class Person implements Greetable {
+  name?: string;
   age: number;
-  name: string;
-  constructor(n: string, a: number) {
+  constructor(n?: string, a: number) {
     this.age = a;
-    this.name = n;
+    if (n) {
+      this.name;
+    }
   }
   greet(phrase: string) {
-    console.log(phrase + " " + this.name);
+    if (this.name) {
+      console.log(phrase + " " + this.name);
+    } else {
+      console.log("Hi");
+    }
   }
 }
 

27da3ed8f381fdcda4789d2e7216b7f873bd1426
alessandro <alessandro.fan@welld.ch>
2021-04-12 15:46:45 +0200

    feat: complete advanced type module

--- AdvanceTypes/src/app.ts
+++ AdvanceTypes/src/app.ts
@@ -20,12 +20,20 @@ type Combinable = string | number;
 type Numeric = boolean | number;
 type Universal = Combinable & Numeric;
 
+function add(a: number, b: number): number;
+function add(a: number, b: string): string;
+function add(a: string, b: string): string;
+function add(a: string, b: number): string;
 function add(a: Combinable, b: Combinable) {
   if (typeof a === "string" || typeof b === "string") {
     return a.toString() + b.toString();
   }
   return a + b;
 }
+
+const result = add("Max ", "Mara");
+
+result.split(" ");
 
 type UnknownEmployee = Employee | Admin;
 function printEmployeeInformation(emp: UnknownEmployee) {
@@ -69,6 +77,60 @@ function useVehicle(vehicle: Vehicle) {
     vehicle.loadCargo(1000);
   }
 }
-
 useVehicle(v1);
 useVehicle(v2);
+
+type Bird = {
+  type: "bird";
+  flyingSpeed: number;
+};
+
+type Horse = {
+  type: "horse";
+  runningSpeed: number;
+};
+
+type Animal = Horse | Bird;
+
+function moveAnimal(animal: Animal) {
+  let speed;
+  switch (animal.type) {
+    case "bird":
+      speed = animal.flyingSpeed;
+    case "horse":
+      //   speed = animal.runningSpeed;
+      speed = 10;
+  }
+  console.log("Moving animal at speed " + speed);
+}
+
+moveAnimal({ type: "horse", runningSpeed: 10 });
+moveAnimal({ type: "bird", flyingSpeed: 10 });
+
+const paragraph = document.getElementById(
+  "message-output"
+)! as HTMLInputElement;
+const userInputElement = <HTMLInputElement>(
+  document.getElementById("user-input-element")!
+);
+
+const userInputElemen2 = document.getElementById("user-input-element2");
+
+paragraph.value = "ciao";
+userInputElement.value = "Hi there";
+
+if (userInputElemen2) {
+  (userInputElemen2 as HTMLInputElement).value = "Hello there again!";
+}
+
+const fetchUserData = {
+  id: "ul",
+  name: "Max",
+    job: { title: "pipe constructor" },
+};
+
+console.log(fetchUserData?.job?.title);
+
+const someVariable = undefined;
+const storedSomeVariable = someVariable ?? "DEFAULT";
+console.log(storedSomeVariable);

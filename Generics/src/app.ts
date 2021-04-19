07cf274ac4f73d6e82879e66555fbf6fbf13b96e
alessandro <alessandro.fan@welld.ch>
2021-04-20 00:00:07 +0200

    feat: type constraints on generics

--- Generics/src/app.ts
+++ Generics/src/app.ts
@@ -1,1 +1,20 @@
-// Code goes here!
+//Example of a Generic which is similar to string[]
+const names: Array<string> = [];
+names.push("ciao", "davide");
+names[0].split(" ");
+
+const promise: Promise<string> = new Promise((resolve) => {
+  setTimeout(() => {
+    resolve("this is done!");
+  }, 2000);
+});
+
+promise.then((data) => data.split(""));
+
+//Generic Functions
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergeObject = merge({ name: "Pierino" }, { age: 2 });
+console.log(mergeObject.name, mergeObject.age); //TypeScript can't know this

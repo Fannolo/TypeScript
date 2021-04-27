39ace9c800cc08b759a885d4f72289cb49022e3f
alessandro <alessandro.fan@welld.ch>
2021-04-27 08:58:13 +0200

    feat: additional generic components

--- Generics/src/app.ts
+++ Generics/src/app.ts
@@ -1,29 +1,8 @@
-//Simple Generic Class for simple men
-class DataStorage<T extends string | number | boolean> {
-  private data: T[] = [];
+//generic utility types
+// can give you extra type safety or extra flexibility type
 
-  addItem(item: T) {
-    this.data.push(item);
-  }
-
-  removeItem<S extends T>(item: S) {
-    this.data.splice(this.data.indexOf(item), 1); // remove 1 element at the index of item
-  }
-  getItems() {
-    return [...this.data];
-  }
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUtil: Date;
 }
-//full flexibility, but we give some information to make it very flexible but still very strong typed
-const stringStorage = new DataStorage<string>();
-stringStorage.addItem("Max");
-stringStorage.addItem("ciccio");
-stringStorage.removeItem("Max");
-console.log(stringStorage.getItems());
-
-//object in javascript are referenced which means they all point to a specific memory allocation
-const objectStorage = new DataStorage<object>(); // objects are not allowed
-objectStorage.addItem({ name: "Max" });
-objectStorage.addItem({ name: "ciccio" });
-//...
-objectStorage.removeItem({ name: "Max" });
-console.log(objectStorage.getItems());
---
+++ Generics/src/generic.classes.ts
@@ -0,0 +1,31 @@
+//Simple Generic Class for simple men
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem<S extends T>(item: S) {
+    this.data.splice(this.data.indexOf(item), 1); // remove 1 element at the index of item
+  }
+  getItems() {
+    return [...this.data];
+  }
+}
+//full flexibility, but we give some information to make it very flexible but still very strong typed
+const stringStorage = new DataStorage<string>();
+stringStorage.addItem("Max");
+stringStorage.addItem("ciccio");
+stringStorage.removeItem("Max");
+console.log(stringStorage.getItems());
+
+//object in javascript are referenced which means they all point to a specific memory allocation
+// const objectStorage = new DataStorage<object>(); // objects are not allowed
+// objectStorage.addItem({ name: "Max" });
+// objectStorage.addItem({ name: "ciccio" });
+// //...
+// objectStorage.removeItem({ name: "Max" });
+// console.log(objectStorage.getItems());
+
+//Generic utility

f7eff5eb9b2aa205e1a99251b0a19317e7d6969e
alessandro <alessandro.fan@welld.ch>
2021-04-30 15:45:58 +0200

    feat: add Decorators

--- Generics/src/app.ts
+++ Generics/src/app.ts
@@ -1,8 +1,28 @@
 //generic utility types
-// can give you extra type safety or extra flexibility type
-
+//can give you extra type safety or extra flexibility type
 interface CourseGoal {
-  title: string;
-  description: string;
-  completeUtil: Date;
+  title?: string;
+  description?: string;
+  completetUntil?: Date;
 }
+
+function createCourseGoal(
+  title: string,
+  description: string,
+  date: Date
+): CourseGoal {
+  //we are taking advantage the partial keyword which means
+  //that we can initialize the property at the beginning whatever
+  //we want and eventually that will be an object of type CourseGoal
+  let courseGoal: Partial<CourseGoal> = {};
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completetUntil = date;
+  //returning as a Type
+  return courseGoal as CourseGoal;
+}
+
+const james: ReadonlyArray<string> = ["max", "mary"];
+
+// james.push("max"); //can't do this it's readonly
+// james.pop(); //can't do this it's readonly

0b14286d2a9410baaa42a34bff5975820aaec91e
alessandro <alessandro.fan@welld.ch>
2021-06-27 17:26:34 +0200

    webpack module and drag & drop exercise

--- Exercise drag&drop/src/useRenderTemplate.ts
+++
@@ -1,8 +0,0 @@
-export default function useRenderTemplate(
-  templates: HTMLTemplateElement[],
-  app: HTMLDivElement
-): void {
-  templates.forEach((template) => {
-    app.appendChild(template.content.cloneNode(true));
-  });
-}
